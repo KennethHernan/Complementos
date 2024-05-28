caa04bc5bb3bdd23ab235b8e8f71cdf7a9bee207
Kenneth Hernan <kdelacruz18@autonoma.edu.pe>
2024-05-28 16:06:28 -0500

    Completo

--- src/script.js
+++ src/script.js
@@ -39,3 +39,19 @@ $('ul.tabs li a:first').addClass('active');
 		$(activeTab).show();
 		return false;
 	});
+
+  //parte paginacion
+  var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+  
